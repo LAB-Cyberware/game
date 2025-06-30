4309644a151f9f8b7a0921b4d8f3166f741e778f
LAB-Cyberware <lab.cyberware@gmail.com>
2025-06-30 18:53:05 -0400

    Animaciones del buildsModal y sus respectivas explicaciones

--- src/components/buildsModal.js
+++ src/components/buildsModal.js
@@ -12,6 +12,19 @@ function BuildsModal({ buildingType, onClose }) {
     const [selectedMission, setSelectedMission] = useState(null);
     const [isModalOpen, setIsModalOpen] = useState(false);
 
+    /* isClosing indica el estado inicial de cierre del Modal. */
+    /* setIsClosing indica el cambio del estado inicial de cierre del Modal, esto
+    pasando el useState a true con el handleClose. */
+    /* useState(false) indica que, claramente el Modal no se está cerrando inicialmente,
+    por lo que cargará las animaciones de cierre al actualizarse a true. */
+    const [isClosing, setIsClosing] = useState(false);
+    const [isVisible] = useState(true);
+    /* isVisible = useState(true) indica que, mientras el componente esté activado, 
+    siempre será visible y no tendrá ningún setIsVisible ni nada por el estilo para
+    cambiar ele estado, pues, haría que este parpadee al iniciar las animaciones, y no
+    funcionaría correctamente, ya de los demás cambios de cierre se encarga el
+    isClosing. */
+
     const handleMissionClick = (missionType) => {
         setSelectedMission(missionType);
         setIsModalOpen(true);
@@ -20,6 +33,23 @@ function BuildsModal({ buildingType, onClose }) {
     const closeModal = () => {
         setIsModalOpen(false);
         setSelectedMission(null);
+    };
+
+    /* handleClose al completo será para indicar el cómo, la forma, de que el Modal se
+    cierra. */
+    /* setIsClosing(true) indica que se activará el setIsClosing visto anteriormente,
+    este pasado a true, el cambio de estado en pocas palabras, para indicar que el Modal
+    se está cerrando y que vaya activando animaciones y así. */
+    /* setTimeout buscará que el cierre (onClose) demore cierta cantidad de tiempo, para
+    el caso presente ha sido introducido el número "300", indicando que la cantidad de
+    tiempo para que cierre el Modal será de 300 milisegundos (0.3 segundos), lo justo
+    para que se active la animación, si no tuviera el setTimeout cerraría todo sin dar
+    tiempo a que las animaciones se activen. */
+    const handleClose = () => {
+        setIsClosing(true);
+        setTimeout(() => {
+            onClose();
+        }, 300);
     };
 
     /* buildingsData actúa como si fuese una base de datos, posee los datos de cada
@@ -86,7 +116,7 @@ function BuildsModal({ buildingType, onClose }) {
     /* || es un operador que significa "o" en este caso dice que, si no encuentra el tipo
     de edificio desde buildingsData, dirá que, de hecho, se trata de el tipo de edificio
     "desconocido" */
-    /* Esta línea está más que todo para evitar errores, no es tremendamente necesaria. */
+    /* Esta línea es perfecta para evitar errores. */
     const currentBuilding = buildingsData[buildingType] || buildingsData.desconocido;
 
     /* handleOverlayClick es igual a un evento (e), y este evento ocurre con lo que sigue
@@ -102,19 +132,36 @@ function BuildsModal({ buildingType, onClose }) {
     ya que cumpliría los requisitos para ello. */
     const handleOverlayClick = (e) => {
         if (e.target === e.currentTarget) {
+        setIsClosing(true);
+        setTimeout(() => {
             onClose();
+        }, 300);
         }
     };
     
     return (
         <>
-        <div className="modal-build-overlay" onClick={handleOverlayClick}>
-            <div className="modal-build-content">
+        <div className={`modal-build-overlay ${isVisible ? 'fade-in' : ''} ${isClosing ? 'fade-out' : ''}`}
+        /* Los {} están agrupando el contenido de forma compacta y así no implementar 
+        tanto código. Y los `` sirven para poder usar expresiones JavaScript dentro del
+        contenido agrupado. */ 
+        /* ${isVisible ? 'fade-in}' : ''} indica que al momento de ser visible el Modal,
+        este activará la animación de fade-in, el : '' sería si hubiera un caso alterno.
+        Para ser más claros, el ? funciona como un if, osea, un si, y el : como un si
+        no, pero para el presente caso no es necesario nada más ya que el isVisible siempre
+        estará en true al activar el componente BuildsModal.  */
+        /* ${isClosing ? 'fade-out'} : ''} indica que al momento de que el Modal se esté
+        cerrando, osea, el isClosing pase a true, este active la animación de fade-out */
+        onClick={handleOverlayClick}>
+            <div className={`modal-build-content ${isVisible ? 'slide-in' :''} ${isClosing ? 'slide-out' : ''}`}
+            /* El mismo funcionamiento que con el modal-build-overlay, pero en el
+            modal-build-content y cambiando los fade-in y fade-out por slide-in y
+            slide-out. */>
                 <div className="modal-build-top">
                     <h2>{currentBuilding.name}</h2>
-                    <button className="modal-build-close" onClick={onClose}
-                    /* onClick={onClose} dice que si se pulsa este botón el Modal se
-                    cierra. */
+                    <button className="modal-build-close" onClick={handleClose}
+                    /* onClick={handleclose} dice que si se pulsa este botón el Modal se
+                    cierra, activando el timeout igualmente. */
                     /* El svg es una imagen de un sitio web, la X para ser exactos. 
                     Al ser svg es bastante modificable, por ello se usa el path, elemento 
                     svg para lograr modificaciones de lineas y curvas en el mismo con
